d2f7e0b27710cb81f1594aef93fa84468a186235
Miao <himia301@gmail.com>
2023-07-15 13:32:03 +0800

    10.10 exercise

--- rate-repository-app/src/theme.js
+++ rate-repository-app/src/theme.js
@@ -1,4 +1,5 @@
 import Constants from 'expo-constants';
+import { Platform } from 'react-native';
 
 const theme = {
     colors: {
@@ -14,7 +15,11 @@ const theme = {
       appBarHeading: 20
     },
     fonts: {
-      main: 'System',
+        main: Platform.select({
+          android: "Roboto",
+          ios: "Arial",
+          default: "System",
+        }),
     },
     fontWeights: {
       normal: '400',
