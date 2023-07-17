53c5baf35a0abc04aff578b017d9cbd3a031652a
Miao <himia301@gmail.com>
2023-07-17 16:11:59 +0800

    10.11 exercise: fetching repositories with Apollo Client

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -3,6 +3,10 @@ import Main from "./src/components/Main";
 import Constants from 'expo-constants';
 import { NativeRouter } from "react-router-native";
 import { StatusBar } from "expo-status-bar";
+import { ApolloProvider } from '@apollo/client';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 const styles = StyleSheet.create({
   container: {
@@ -17,14 +21,14 @@ const App = () => {
   return (
     <>
       <NativeRouter>
+       <ApolloProvider client={apolloClient}>
         <View style={styles.container}>
           <Main />
         </View>
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar style='auto' />
     </>
-    
-  
   )
 }
 
--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -1,24 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { loading, data } = useQuery(GET_REPOSITORIES, 
+    {
+      fetchPolicy: 'cache-and-network',
+    }
+  );
 
-  const fetchRepositories = async () => {
-    setLoading(true);
+  if (loading) return <div>Loading ...</div>;
 
-    const response = await fetch('http://192.168.5.171:5001/api/repositories');
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories: data?.repositories };
 };
 
 export default useRepositories;
